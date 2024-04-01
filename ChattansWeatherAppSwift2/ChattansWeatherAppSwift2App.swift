262b4de435ba49875614b27d1b7589c7f1ba2a69
Emy Alwin <emy.alwin@gmail.com>
2024-04-01 14:48:43 +0200

    Initial Commit

---
+++ ChattansWeatherAppSwift2/ChattansWeatherAppSwift2App.swift
@@ -0,0 +1,17 @@
+//
+//  ChattansWeatherAppSwift2App.swift
+//  ChattansWeatherAppSwift2
+//
+//  Created by Emy Alwin on 2024-04-01.
+//
+
+import SwiftUI
+
+@main
+struct ChattansWeatherAppSwift2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
